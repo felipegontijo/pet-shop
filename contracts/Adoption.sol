4129fa11a043563e9969742c28a802fcc0d4e595
Felipe Gontijo <epilef.gontijo@gmail.com>
2020-09-08 11:19:56 -0300

    feat: Adopt a pet

---
+++ contracts/Adoption.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Adoption {
+    address[16] public adopters; // an array of ethereum addresses of length 16
+
+    // In solidity the types of both the function parameters and output must be specified
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15); // ensure petId is in the range of our adopters array
+        adopters[petId] = msg.sender; // msg.sender denotes the address which called this function
+        return petId;
+    }
+}
